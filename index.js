9d0c0f0f60f9cd0cf0b7bdea78b5e4e474c23dab
jbeckmann1 <jo.bec@t-online.de>
2020-12-27 06:19:02 +0100

    Start function

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
+//Debounce function
+const debounce = require('lodash.debounce');
 //chokidar watches for new/changed files
 const chokidar = require('chokidar');
 //CLI Tool
 const caporal = require('caporal');
+
+const start = debounce(() => {
+	console.log('Starting Users Program');
+}, 100);
+
 chokidar
 	.watch('.')
-	.on('add', () => console.log('File added'))
+	.on('add', start)
 	.on('change', () => console.log('File changed'))
 	.on('unlink', () => console.log('File unlinked'));
 console.log('hi');
