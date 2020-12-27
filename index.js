96d655b922b70309d14d20103de191e19a465b88
jbeckmann1 <jo.bec@t-online.de>
2020-12-27 06:59:18 +0100

    Check if file exists

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 //CLI Tool
 const program = require('caporal');
-const { parse } = require('caporal');
+const fs = require('fs');
 
-program.version('0.0.1').argument('[filename]', 'Name of a file to execute').action((args) => {
-	console.log(args);
+program.version('0.0.1').argument('[filename]', 'Name of a file to execute').action(async ({ filename }) => {
+	const name = filename || 'index.js';
+
+	try {
+		await fs.promises.access(name);
+	} catch (err) {
+		throw new Error(`could not find the file ${name}`);
+	}
+	const start = debounce(() => {
+		console.log('Starting Users Program');
+	}, 500);
+
+	chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
 });
 
 program.parse(process.argv);
-
-const start = debounce(() => {
-	console.log('Starting Users Program');
-}, 100);
-
-chokidar
-	.watch('.')
-	.on('add', start)
-	.on('change', () => console.log('File changed'))
-	.on('unlink', () => console.log('File unlinked'));
