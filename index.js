2a70a4d049b7c385ce6690689634f936da5cc251
jbeckmann1 <jo.bec@t-online.de>
2020-12-27 06:49:40 +0100

    Implement CLI

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const debounce = require('lodash.debounce');
 //chokidar watches for new/changed files
 const chokidar = require('chokidar');
 //CLI Tool
-const caporal = require('caporal');
+const program = require('caporal');
+const { parse } = require('caporal');
+
+program.version('0.0.1').argument('[filename]', 'Name of a file to execute').action((args) => {
+	console.log(args);
+});
+
+program.parse(process.argv);
 
 const start = debounce(() => {
 	console.log('Starting Users Program');
@@ -15,4 +22,3 @@ chokidar
 	.on('add', start)
 	.on('change', () => console.log('File changed'))
 	.on('unlink', () => console.log('File unlinked'));
-console.log('hi');
