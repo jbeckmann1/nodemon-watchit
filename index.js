3282e81279047cef6cb04cedab42d337909eaa10
jbeckmann1 <jo.bec@t-online.de>
2020-12-27 06:18:41 +0100

    Watch for changes and implement start function

--- index.js
+++ index.js
@@ -3,4 +3,9 @@
 const chokidar = require('chokidar');
 //CLI Tool
 const caporal = require('caporal');
+chokidar
+	.watch('.')
+	.on('add', () => console.log('File added'))
+	.on('change', () => console.log('File changed'))
+	.on('unlink', () => console.log('File unlinked'));
 console.log('hi');
