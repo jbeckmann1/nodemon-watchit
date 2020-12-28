6f4c265488d47151daefc92c62a0495a42e5c593
jbeckmann1 <jo.bec@t-online.de>
2020-12-28 09:41:08 +0100

    Implement termination

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
 const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program.version('0.0.1').argument('[filename]', 'Name of a file to execute').action(async ({ filename }) => {
 	const name = filename || 'index.js';
@@ -16,9 +17,14 @@ program.version('0.0.1').argument('[filename]', 'Name of a file to execute').act
 	} catch (err) {
 		throw new Error(`could not find the file ${name}`);
 	}
+	let proc;
 	const start = debounce(() => {
-		spawn('node', [ name ], { stdio: 'inherit' });
-	}, 500);
+		if (proc) {
+			proc.kill();
+		}
+		console.log(chalk.blue('>>> Starting process...'));
+		proc = spawn('node', [ name ], { stdio: 'inherit' });
+	}, 200);
 
 	chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
 });
